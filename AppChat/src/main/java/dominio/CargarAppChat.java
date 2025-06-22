2d31eb5801c4e2cf6cd36f4b582aa36d8fddad9d
Vicente González Morales <vicente.gnzmls@gmail.com>
2025-06-22 10:50:37 +0200

    Lanzador de la app

--- AppChat/src/main/java/dominio/CargarAppChat.java
+++ AppChat/src/main/java/dominio/CargarAppChat.java
@@ -7,26 +7,21 @@ public class CargarAppChat {
 
 	public static void cargar() {
 		AppChat appChat = AppChat.INSTANCE;
-		appChat.registrarUsuario("aa", "aa", "11", "aa", "Hola, soy jesus", LocalDate.of(1960, 10, 03),"/usuarios/user.jpeg");
-		appChat.registrarUsuario("bb","bb", "22", "bb", "hola, soy elena", LocalDate.of(1995, 12, 28), "/usuarios/user.jpeg");
-		appChat.registrarUsuario("cc", "cc", "33", "cc", "hola, soy rosalia", LocalDate.of(2000, 5, 15), "/usuarios/user.jpeg");
-		appChat.registrarUsuario("dd", "dd", "44", "dd", "hola, soy diego", LocalDate.of(1970, 5, 11), "/usuarios/user.jpeg");
-		appChat.registrarUsuario("ee", "ee", "55", "ee", "hola, soy anne", LocalDate.of(1990, 3, 28), "/usuarios/user.jpeg");
+		appChat.registrarUsuario("Jesús", "Martínez", "11", "11", "Hola, soy Jesus", LocalDate.of(1960, 10, 03),"/usuarios/user.jpeg");
+		appChat.registrarUsuario("Elena","Carrasco", "22", "22", "Hola, soy Elena", LocalDate.of(1995, 12, 28), "/usuarios/user.jpeg");
+		appChat.registrarUsuario("Rosalía", "Hernández", "33", "33", "Hola, soy Rosalia", LocalDate.of(2000, 5, 15), "/usuarios/user.jpeg");
+		appChat.registrarUsuario("Diego", "Belmonte", "44", "44", "Hola, soy Diego", LocalDate.of(1970, 5, 11), "/usuarios/user.jpeg");
+		appChat.registrarUsuario("Anne", "Smith", "55", "55", "Hola, soy Anne", LocalDate.of(1990, 3, 28), "/usuarios/user.jpeg");
 		
-		appChat.login("11", "aa");
-		
-		ContactoIndividual c2 = appChat.agregarContacto("elena", "22");
-		ContactoIndividual c3 = appChat.agregarContacto("rosalia", "33");
+		ContactoIndividual c2 = appChat.agregarContacto("Elena", "22");
+		ContactoIndividual c3 = appChat.agregarContacto("Rosalía", "33");
 		
 		appChat.enviarMensajeContacto(c2, "Hola, ¿cómo estás?", -1, TipoMensaje.ENVIADO);
 		appChat.enviarMensajeContacto(c2, "", 2, TipoMensaje.ENVIADO);
 		
 		appChat.enviarMensajeContacto(c3, "Cuando cantas?", -1, TipoMensaje.ENVIADO);
 		appChat.enviarMensajeContacto(c2, "", 6, TipoMensaje.ENVIADO);
-		
-		appChat.login("22", "bb");
-		
-		//ContactoIndividual c1 =appChat.agregarContacto("jesus", "11");
+				
 		ContactoIndividual c1 = RepositorioUsuarios.INSTANCE.buscarUsuarioPorTelefono("22").buscarContactoIndividual("11");
 		ContactoIndividual c4 = appChat.agregarContacto("diego", "44");
 		ContactoIndividual c5 = appChat.agregarContacto("anne", "55");
